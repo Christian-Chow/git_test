f22e0e28d51eca5b99516996a041d67e8a47722d
Christian Chow <114394138+Christian-Chow@users.noreply.github.com>
2025-03-29 22:28:53 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/git_test/MainActivity.kt
+++ app/src/main/java/com/example/git_test/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                 //experimental code blablabla
                 //hehehaha
                 //local changes
+                //changes from github !!!
             }
         }
     }
